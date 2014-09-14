fd38eb3fae528234573fb60d4d9986b35035575d
Vital <vitaliykastsiuk@gmail.com>
2014-09-14 21:25:17 +0300

    useless

--- Graph_edit/custom_rectangle.cpp
+++
@@ -1,8 +0,0 @@
-#include "custom_rectangle.h"
-
-CustomRectangle::CustomRectangle(int newX, int newY) : CustomShape(newX, newY){}
-
-void CustomRectangle::draw(HDC &dc, int newX, int newY)
-{
-	Rectangle(dc, x, y, newX, newY);
-}
