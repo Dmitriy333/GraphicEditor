edb3db45d948817662d63d8865d05a23ed678de8
Vital <vitaliykastsiuk@gmail.com>
2014-10-04 21:26:43 +0300

    junk

--- Graph_edit/figures.h
+++ Graph_edit/figures.h
@@ -2,7 +2,7 @@
 
 #include "custom_shapes.h"
 
-enum draw { CURRENT, BUFFER, BACKUP, RESTORE, MOVE};
+enum draw { CURRENT, BUFFER, BACKUP, RESTORE };
 enum Tools { PEN, LINE, RECTANGLE, ELLIPSE, POLY, TEXT, NONE };
 
 #define BACKUPS 11
